068245135abde0db279ac9c67c9f58ed9ce83576
Noah Hollowell <noahdhollowell@googlemail.com>
2023-01-23 13:24:37 +0000

    Added icon to all pages

--- resources/views/public-results/view-serc.blade.php
+++ resources/views/public-results/view-serc.blade.php
@@ -3,6 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
+    <link rel="icon" type="image/png" href="{{ asset('blogo.png') }}" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $event->getName() }} | {{ $comp->name }} | Results | BULSCA</title>
