ed7aba86ef0b900033d0ec566d0698adebd6db94
Noah Hollowell <noahdhollowell@googlemail.com>
2023-04-02 18:16:45 +0100

    Fixed improperly resizing confirm judge text

--- resources/views/digitaljudge/judging/confirm-judge.blade.php
+++ resources/views/digitaljudge/judging/confirm-judge.blade.php
@@ -1,15 +1,15 @@
 @extends('digitaljudge.layout')
 
 @section('content')
-<div class=" w-screen flex flex-col items-center mt-8 space-y-4">
+<div class=" w-screen flex flex-col items-center mt-8 space-y-4 px-4">
 
-    <div class="flex flex-col space-y-4 items-center mb-6">
+    <div class="flex flex-col space-y-4 items-center mb-6 w-full">
         <img src="{{ asset('blogo.png') }}" alt="BULSCA Logo" class=" w-52 h-52 ">
         <h5 class="font-semibold ">DigitalJudge | {{ $comp->name }} | {{ $serc->getName() }}</h5>
         <br>
-        <h2 class="font-bold">{{ $judge->name }}</h2>
+        <h2 class="font-bold text-center w-full break-words">{{ $judge->name }}</h2>
 
-        <p class="px-4">Please check the judging criteria below matches your breif and casualty and then click "Continue"</p>
+        <p class="">Please check the judging criteria below matches your breif and casualty and then click "Continue"</p>
 
         <h4>Criteria</h4>
         <ul class=" list-disc ">
@@ -20,11 +20,11 @@
             @endforeach
         </ul>
         <br>
-        <form action="{{ route('dj.judging.confirm', $judge) }}" method="post" class=" w-[80%]">
+        <form action="{{ route('dj.judging.confirm', $judge) }}" method="post" class=" ">
             @csrf
             <button type="submit" class="btn w-full">Continue</button>
         </form>
-        <a href="{{ route('dj.home') }}" class="btn btn-danger w-[80%]">Back</a>
+        <a href="{{ route('dj.home') }}" class="btn btn-danger ">Back</a>
 
     </div>
 </div>
