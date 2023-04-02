361b33c5fa61d66d2150db6aa1493fcdcce16347
Noah Hollowell <noahdhollowell@googlemail.com>
2023-04-02 18:01:03 +0100

    Removed start judging button for head ref Added back button to judging page

--- resources/views/digitaljudge/judging/home.blade.php
+++ resources/views/digitaljudge/judging/home.blade.php
@@ -17,6 +17,8 @@
             @endforelse
         </h2>
 
+        <a href="{{ route('dj.home') }}" class="link">Back</a>
+
         <p class="px-4">There are X teams to Judge. To start judging press "Start Judging", if you need to judge multiple judges then click 'Add Judge' below.</p>
         <p class="px-4">You will not be able to edit/see scores after submitting them, however you will be able to see you highest, lowest and average score awarded for each criteria at all times.</p>
         <p class="px-4">If you need to get back to the judging page, click "Start Judging" again. It will resume you at the last team you started to judge!</p>
@@ -28,12 +30,14 @@
         @if (count($judges) > 1)
         <a href="{{ route('dj.judging.remove-judge') }}" class="link">Remove Judge</a>
         @endif
+
+        <a href="{{ route('dj.judging.next-team') }}" class="btn w-[80%]">Start Judging</a>
         @endif
 
 
 
 
-        <a href="{{ route('dj.judging.next-team') }}" class="btn w-[80%]">Start Judging</a>
+
 
         <h4>Team Order</h4>
         <ul class=" list-decimal ">
