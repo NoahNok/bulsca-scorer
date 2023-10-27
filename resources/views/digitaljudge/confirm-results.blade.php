89052024d7ba766a450021467fe58f5e7402780c
Noah Hollowell <noahdhollowell@googlemail.com>
2023-10-27 16:08:15 +0100

    Add penalties and DQ to SERC results confirmation

--- resources/views/digitaljudge/confirm-results.blade.php
+++ resources/views/digitaljudge/confirm-results.blade.php
@@ -19,7 +19,7 @@
 
 
             @foreach ($serc->getTeams() as $team)
-                <div class="flex flex-col w-full md:w-auto">
+                <div class="flex flex-col w-full md:w-[30%]">
                     <h3>{{ $team->getFullname() }}</h3>
 
                     <div class="flex flex-col pb-8">
@@ -40,7 +40,31 @@
                                             </div>
                                         </div>
                                     @endforeach
+                                    @php
+                                        $dq = $serc->getTeamDQ($team);
+                                        $penalties = $serc->getTeamPenalties($team);
+                                    @endphp
+
+                                    @if ($penalties)
+                                        <div
+                                            class="flex items-center border-b-2 border-x-2 bg-gray-300 border-gray-200 first-of-type:border-t-2 ">
+                                            <div class="w-[60%] bg-gray-300 ">Penalties</div>
+                                            <div class="ml-auto text-right pr-6 w-[40%] bg-white break-words ">
+                                                {{ $penalties->codes }}
+                                            </div>
+                                        </div>
+                                    @endif
+
+                                    @if ($dq)
+                                        <div
+                                            class="flex items-center border-b-2 border-x-2 border-gray-200 first-of-type:border-t-2 ">
+                                            <div class="w-[60%] bg-gray-300 p-2">DQ</div>
+                                            <div class="ml-auto text-right pr-6">{{ $dq->code }}
+                                            </div>
+                                        </div>
+                                    @endif
                                 </div>
+
                             </div>
                         @endforeach
                     </div>
