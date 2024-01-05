d1b4b30f93e8c573217691579e1c192896da49d2
Noah Hollowell <noahdhollowell@googlemail.com>
2024-01-05 15:18:40 +0000

    Sidebar can now collapse better and dropdowns now work

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,6 +21,9 @@ module.exports = {
                 "3xl": "1600px",
                 "4xl": "1920px",
             },
+            transitionProperty: {
+                width: "width",
+            },
         },
     },
 
