07bc655b87f922ed90a2121e840be82d833193d9
Noah Hollowell <noahdhollowell@googlemail.com>
2024-01-05 15:20:49 +0000

    Fix missing import for JudgeNote

--- app/Models/SERC.php
+++ app/Models/SERC.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\DigitalJudge\JudgeNote;
 use App\Models\Interfaces\IPenalisable;
 use App\Traits\Cloneable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
