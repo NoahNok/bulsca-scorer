8d0444e35b103f428718478c1a0ac445f074edc0
Noah Hollowell <noahdhollowell@googlemail.com>
2024-01-08 22:27:46 +0000

    Club tables now scroll on smaller devices Updated mobile sizing

--- resources/views/public-results/stats/club.blade.php
+++ resources/views/public-results/stats/club.blade.php
@@ -12,9 +12,14 @@
 </head>
 
 <body class="overflow-x-hidden flex w-full h-full justify-center">
-    <div class=" w-[75%] my-28 ">
+    <div class=" w-full md:w-[75%] m-6 md:my-28 md:mx-0 ">
         <img src="https://www.bulsca.co.uk/storage/logo/blogo.png" class=" w-40 mb-2 " alt="">
-        <h1 class="font-bold ">{{ $club->name }}</h1>
+
+        <a href="{{ route('public.results.stats.clubs') }}"
+            class="link flex items-center space-x-1  z-50 cursor-pointer">All Clubs</a>
+
+        <h1 class="font-bold  " style="font">{{ $club->name }}</h1>
+
 
 
         <div class="flex divide-x ">Teams:
@@ -28,32 +33,33 @@
         <div class="grid-4">
             <div class="card">
                 <h3>Club Records</h3>
-                <table class="table-auto">
-                    <thead class="text-left">
-                        <tr>
-                            <th>Event</th>
-                            <th>Time</th>
-                            <th>Team</th>
-                            <th>Competition</th>
-
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($club->getClubRecords() as $record)
-                            <tr>
-                                @php
-
-                                @endphp
-                                <td>{{ $record['se'] }}</td>
-                                <td>{{ $record['result'] == 99999999999999999 ? '-' : App\Models\SpeedResult::getPrettyTime($record['result']) }}
-                                </td>
-                                <td>{{ $record['team'] ?? '-' }}</td>
-                                <td>{{ $record['comp_name'] ?? '-' }}</td>
+                <div class="w-full h-full overflow-x-auto">
+                    <table class="table-auto">
+                        <thead class="text-left">
+                            <tr class="gap-1">
+                                <th class="px-2 pl-0">Event</th>
+                                <th class="px-2">Time</th>
+                                <th class="px-2">Team</th>
+                                <th class="px-2">Competition</th>
 
                             </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            @foreach ($club->getClubRecords() as $record)
+                                <tr class=" space">
+
+                                    <td class="px-2 pl-0 whitespace-nowrap">{{ $record['se'] }}</td>
+                                    <td class="px-2">
+                                        {{ $record['result'] == 99999999999999999 ? '-' : App\Models\SpeedResult::getPrettyTime($record['result']) }}
+                                    </td>
+                                    <td class="px-2">{{ $record['team'] ?? '-' }}</td>
+                                    <td class="px-2">{{ $record['comp_name'] ?? '-' }}</td>
+
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
             </div>
             <div class="card">
                 <h3>Competed at</h3>
@@ -67,39 +73,44 @@
             </div>
             <div class="card">
                 <h3>Best SERCs</h3>
-                <table class="table-auto">
-                    <thead class="text-left">
-                        <tr>
-                            <th>SERC</th>
-                            <th>Points</th>
-                            <th>Team</th>
-                            <th>Competition</th>
-
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($club->getBestSercs() as $serc)
+                <div class="w-full h-full overflow-x-auto">
+                    <table class="table-auto">
+                        <thead class="text-left">
                             <tr>
-                                @php
-
-                                @endphp
-                                <td><a href="{{ route('public.results.serc', [$serc->comp_name . '.' . $serc->comp_id, $serc->serc_id]) }}"
-                                        class="link">{{ $serc->serc_name }}</a>
-                                </td>
-                                <td>{{ round($serc->total) }}/{{ round($serc->max) }}
-                                    (<strong>{{ round(($serc->total / $serc->max) * 100, 2) }}%</strong>)
-                                </td>
-                                <td>{{ $serc->team }}</td>
-                                <td><a href="{{ route('public.results.comp', $serc->comp_name . '.' . $serc->comp_id) }}"
-                                        class="link  whitespace-nowrap">
-                                        {{ $serc->comp_name }}
-                                    </a></li>
-                                </td>
+                                <th class="px-2 pl-0">SERC</th>
+                                <th class="px-2">Points</th>
+                                <th class="px-2">Team</th>
+                                <th class="px-2">Competition</th>
 
                             </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            @foreach ($club->getBestSercs() as $serc)
+                                <tr>
+                                    @php
+
+                                    @endphp
+                                    <td class="px-2 pl-0"><a
+                                            href="{{ route('public.results.serc', [$serc->comp_name . '.' . $serc->comp_id, $serc->serc_id]) }}"
+                                            class="link">{{ $serc->serc_name }}</a>
+                                    </td>
+                                    <td class="px-2 whitespace-nowrap">
+                                        {{ round($serc->total) }}/{{ round($serc->max) }}
+                                        (<strong>{{ round(($serc->total / $serc->max) * 100, 2) }}%</strong>)
+                                    </td>
+                                    <td class="px-2">{{ $serc->team }}</td>
+                                    <td class="px-2"><a
+                                            href="{{ route('public.results.comp', $serc->comp_name . '.' . $serc->comp_id) }}"
+                                            class="link  whitespace-nowrap">
+                                            {{ $serc->comp_name }}
+                                        </a></li>
+                                    </td>
+
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
 
 
 
