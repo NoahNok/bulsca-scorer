b7c96636ba2aa4a19aa0014fd171be9e52460a6a
Noah Hollowell <noahdhollowell@googlemail.com>
2024-01-23 21:59:28 +0000

    Force css regen

--- resources/views/competition/events/speeds/view.blade.php
+++ resources/views/competition/events/speeds/view.blade.php
@@ -21,7 +21,7 @@
     </div>
     <div>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-            class="w-3 h-3">
+            class="w-3 h-3 ">
             <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
         </svg>
         <a href="{{ route('comps.view.events', $comp) }}">Events</a>
@@ -39,7 +39,7 @@
     <div class="grid-2">
         <div class="flex flex-col space-y-4">
             <div class="flex justify-between">
-                <h2 class="mb-0">{{ $event->getName() }}</h2>
+                <h2 class="mb-0 mt-0">{{ $event->getName() }}</h2>
                 <a href="{{ route('comps.view.events.speeds.edit', [$comp, $event]) }}" class="btn">Edit</a>
             </div>
 
