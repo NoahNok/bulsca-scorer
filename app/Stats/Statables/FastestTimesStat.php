ba746e556fac4b3387dacf07d0a680a1cc6f58e7
Noah Hollowell <noahdhollowell@googlemail.com>
2024-04-19 13:08:10 +0100

    Update fts

--- app/Stats/Statables/FastestTimesStat.php
+++ app/Stats/Statables/FastestTimesStat.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\DB;
 class FastestTimesStat extends Statable
 {
 
-    private string $baseQuery = "SELECT c.name AS competition, c.id AS competition_id, se.id AS event, cl.name AS club, ct.team, t1.time, t1.points, t1.place FROM stats_times t1 JOIN (SELECT event, MIN(time) AS fastest_time FROM stats_times GROUP BY event) t2 ON t1.event=t2.event AND t1.time=t2.fastest_time INNER JOIN
+    private string $baseQuery = "SELECT c.name AS competition, c.id AS competition_id, se.id AS event, cl.name AS club, ct.team, t1.time, t1.points, t1.place FROM stats_times t1 INNER JOIN (SELECT event, MIN(time) AS fastest_time FROM stats_times GROUP BY event) t2 ON t1.event=t2.event AND t1.time=t2.fastest_time INNER JOIN
     speed_events se ON se.id=t1.event INNER JOIN competitions c ON c.id=t1.competition INNER JOIN competition_teams ct ON ct.id=t1.team INNER JOIN clubs cl ON cl.id=ct.club :WHERE: ORDER BY t1.event";
 
     protected string $templateName = "fastest-times";
