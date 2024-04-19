f8baa9dcccbf9589059b4f43e9a3e94f4637f390
Noah Hollowell <noahdhollowell@googlemail.com>
2024-04-19 13:35:24 +0100

    Fixed fastest times query

--- app/Stats/Statables/FastestTimesStat.php
+++ app/Stats/Statables/FastestTimesStat.php
@@ -11,8 +11,8 @@ use Illuminate\Support\Facades\DB;
 class FastestTimesStat extends Statable
 {
 
-    private string $baseQuery = "SELECT c.name AS competition, c.id AS competition_id, se.id AS event, cl.name AS club, ct.team, t1.time, t1.points, t1.place FROM stats_times t1 INNER JOIN (SELECT event, MIN(time) AS fastest_time FROM stats_times GROUP BY event) t2 ON t1.event=t2.event AND t1.time=t2.fastest_time INNER JOIN
-    speed_events se ON se.id=t1.event INNER JOIN competitions c ON c.id=t1.competition INNER JOIN competition_teams ct ON ct.id=t1.team INNER JOIN clubs cl ON cl.id=ct.club :WHERE: ORDER BY t1.event";
+    private string $baseQuery = "SELECT * FROM (SELECT *, RANK() OVER (PARTITION BY event ORDER BY time) AS r FROM (
+        SELECT c.name AS competition, c.id AS competition_id, se.id AS event, cl.name AS club, ct.team, t.time, t.points, t.place FROM stats_times t INNER JOIN competitions c ON c.id=t.competition INNER JOIN speed_events se ON se.id=t.event INNER JOIN competition_teams ct ON ct.id=t.team INNER JOIN clubs cl ON cl.id=ct.club WHERE time > 4 :WHERE: ORDER BY event) tt) ttt WHERE ttt.r=1;";
 
     protected string $templateName = "fastest-times";
 
@@ -25,12 +25,12 @@ class FastestTimesStat extends Statable
 
     public function forClub(Club $club): array
     {
-        return DB::select(str_replace(":WHERE:", "  WHERE cl.id = ?", $this->baseQuery), [$club->id]);
+        return DB::select(str_replace(":WHERE:", "  AND cl.id = ?", $this->baseQuery), [$club->id]);
     }
 
     public function forTeam(Club $club, string $team): array
     {
-        return DB::select(str_replace(":WHERE:", "WHERE cl.id = ? AND ct.team = ?", $this->baseQuery), [$club->id, $team]);
+        return DB::select(str_replace(":WHERE:", " AND cl.id = ? AND ct.team = ?", $this->baseQuery), [$club->id, $team]);
     }
 
 
