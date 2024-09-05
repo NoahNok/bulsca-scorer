b27861e0611b5f3289f51dd95f2dc0e0d93be8e9
Noah Hollowell <noahdhollowell@googlemail.com>
2024-09-05 21:42:29 +0100

    add tanks to live page needs caching cus its slow look at what queries are doing

--- resources/views/live/nationals.blade.php
+++ resources/views/live/nationals.blade.php
@@ -24,7 +24,7 @@
 <body class="overflow-x-hidden  w-screen h-screen">
 
 
-    <div class="w-full h-48 bg-rlss-blue flex flex-col justify-center px-12 "
+    <div class="w-full h-48 bg-rlss-blue flex flex-col justify-center px-12 overflow-x-hidden "
         style="background-image: url('/rlss-transparent.svg'); background-position-y: center; background-position-x: -100px; background-repeat: no-repeat;">
 
         <div class="container mx-auto">
@@ -35,112 +35,180 @@
 
     </div>
 
-    <div class="container mx-auto py-6">
-        <h2 class="text-astoria text-rlss-blue font-extrabold">Event Order - Diving Pit Side</h2>
+    <div class="container mx-auto py-6 overflow-x-hidden">
+
+        <div class="w-full max-w-full flex  px-2">
+            <a href="#tanks" class="btn !bg-rlss-blue mx-2 grow  ">Jump to tanks</a>
+        </div>
+
+
+        <br>
+
+        <h2 class="font-astoria text-rlss-blue font-extrabold">Event Order - Diving Pit Side</h2>
 
         @php
             $heats = $comp->getHeatEntries->sortBy(['heat', 'lane'])->groupBy('heat');
         @endphp
 
-        <table class="table-auto font-greycliff">
-            <thead>
-                <tr class=" text-rlss-blue font-bold">
-                    <th class="bg-rlss-blue py-2 px-3 border border-rlss-blue"></th>
-                    <th class=" bg-rlss-blue bg-opacity-40 py-2 px-3 border border-rlss-blue w-48">Category</th>
-                    <th class="border border-rlss-blue w-48">Lane 1</th>
-                    <th class="border border-rlss-blue w-48">Lane 2</th>
-                    <th class="border border-rlss-blue w-48">Lane 3</th>
-                    <th class="border border-rlss-blue w-48">Lane 4</th>
-                    <th class="border border-rlss-blue w-48">Lane 5</th>
-                    <th class="border border-rlss-blue w-48">Lane 6</th>
-                    <th class="border border-rlss-blue w-48">Lane 7</th>
-                    <th class="border border-rlss-blue w-48">Lane 8</th>
-                </tr>
-            </thead>
-            <tbody>
-                @forelse ($heats->nth(2) as $key => $heat)
-                    <tr>
-                        <td class=" bg-rlss-blue text-white  text-center py-2 px-3 border border-rlss-blue ">
-                            {{ $heat->first()->heat }}</td>
-                        <td
-                            class="py-2 px-3 bg-rlss-blue bg-opacity-40 text-rlss-blue border border-rlss-blue text-center">
-                            {{ $heat->where('lane', 4)->first()->getTeam->getLeague->name }}
-                        </td>
-
-                        @for ($l = 1; $l <= $comp->max_lanes; $l++)
-                            @php
-                                $lane = $heat->where('lane', $l)->first();
-                            @endphp
-
-
-                            <td class="py-2 px-3 text-center border border-rlss-blue">
-                                @if ($lane)
-                                    {{ $lane->getTeam->formatName(':N') }}
-                                @else
-                                    -
-                                @endif
+        <div class="w-full overflow-x-auto  ">
+            <table class="table-auto font-greycliff">
+                <thead>
+                    <tr class=" text-rlss-blue font-bold  ">
+                        <th class="bg-rlss-blue py-2 px-3 border border-rlss-blue sticky left-0 "></th>
+                        <th class=" bg-rlss-blue bg-opacity-40 py-2 px-3 border border-rlss-blue w-48  ">
+                            Category</th>
+                        <th class="border border-rlss-blue w-48">Lane 1</th>
+                        <th class="border border-rlss-blue w-48">Lane 2</th>
+                        <th class="border border-rlss-blue w-48">Lane 3</th>
+                        <th class="border border-rlss-blue w-48">Lane 4</th>
+                        <th class="border border-rlss-blue w-48">Lane 5</th>
+                        <th class="border border-rlss-blue w-48">Lane 6</th>
+                        <th class="border border-rlss-blue w-48">Lane 7</th>
+                        <th class="border border-rlss-blue w-48">Lane 8</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse ($heats->nth(2) as $key => $heat)
+                        <tr class="relative">
+                            <td
+                                class=" bg-rlss-blue text-white  text-center py-2 px-3 border border-rlss-blue sticky left-0 ">
+                                {{ $heat->first()->heat }}</td>
+                            <td
+                                class="py-2 px-3 bg-rlss-blue bg-opacity-40 text-rlss-blue border border-rlss-blue text-center  ">
+                                {{ $heat->where('lane', 4)->first()->getTeam->getLeague->name }}
                             </td>
-                        @endfor
-                    </tr>
-                @empty
-                    <tr>
-                        <td>No heats available yet!</td>
-                    </tr>
-                @endforelse
-            </tbody>
-        </table>
 
+                            @for ($l = 1; $l <= $comp->max_lanes; $l++)
+                                @php
+                                    $lane = $heat->where('lane', $l)->first();
+                                @endphp
+
+
+                                <td class="py-2 px-3 text-center border border-rlss-blue">
+                                    @if ($lane)
+                                        {{ $lane->getTeam->formatName(':N') }}
+                                    @else
+                                        -
+                                    @endif
+                                </td>
+                            @endfor
+                        </tr>
+                    @empty
+                        <tr>
+                            <td>No heats available yet!</td>
+                        </tr>
+                    @endforelse
+                </tbody>
+            </table>
+        </div>
 
         <br>
-        <h2 class="text-astoria text-rlss-blue font-extrabold ">Event Order - Scoreboard Side </h2>
-
-        <table class="table-auto font-greycliff">
-            <thead>
-                <tr class=" text-rlss-blue font-bold">
-                    <th class="bg-rlss-blue py-2 px-3 border border-rlss-blue"></th>
-                    <th class=" bg-rlss-blue bg-opacity-40 py-2 px-3 border border-rlss-blue w-48">Category</th>
-                    <th class="border border-rlss-blue w-48 ">Lane 1</th>
-                    <th class="border border-rlss-blue w-48">Lane 2</th>
-                    <th class="border border-rlss-blue w-48">Lane 3</th>
-                    <th class="border border-rlss-blue w-48">Lane 4</th>
-                    <th class="border border-rlss-blue w-48">Lane 5</th>
-                    <th class="border border-rlss-blue w-48">Lane 6</th>
-                    <th class="border border-rlss-blue w-48">Lane 7</th>
-                    <th class="border border-rlss-blue w-48">Lane 8</th>
-                </tr>
-            </thead>
-            <tbody>
-                @forelse ($heats->nth(2,1) as $key => $heat)
-                    <tr>
-                        <td class=" bg-rlss-blue text-white  text-center py-2 px-3 border border-rlss-blue ">
-                            {{ $heat->first()->heat }}</td>
-                        <td
-                            class="py-2 px-3 bg-rlss-blue bg-opacity-40 text-rlss-blue border border-rlss-blue text-center">
-                            {{ $heat->where('lane', 4)->first()->getTeam->getLeague->name }}
-                        </td>
-
-                        @for ($l = 1; $l <= $comp->max_lanes; $l++)
-                            @php
-                                $lane = $heat->where('lane', $l)->first();
-                            @endphp
-
-
-                            <td class="py-2 px-3 text-center border border-rlss-blue">
-                                @if ($lane)
-                                    {{ $lane->getTeam->formatName(':N') }}
-                                @else
-                                    -
-                                @endif
+        <h2 class="font-astoria text-rlss-blue font-extrabold ">Event Order - Scoreboard Side </h2>
+        <div class="w-full overflow-x-auto ">
+            <table class="table-auto font-greycliff">
+                <thead>
+                    <tr class=" text-rlss-blue font-bold sticky top-0 left-0">
+                        <th class="bg-rlss-blue py-2 px-3 border border-rlss-blue sticky left-0"></th>
+                        <th class=" bg-rlss-blue bg-opacity-40 py-2 px-3 border border-rlss-blue w-48 ">
+                            Category</th>
+                        <th class="border border-rlss-blue w-48 ">Lane 1</th>
+                        <th class="border border-rlss-blue w-48">Lane 2</th>
+                        <th class="border border-rlss-blue w-48">Lane 3</th>
+                        <th class="border border-rlss-blue w-48">Lane 4</th>
+                        <th class="border border-rlss-blue w-48">Lane 5</th>
+                        <th class="border border-rlss-blue w-48">Lane 6</th>
+                        <th class="border border-rlss-blue w-48">Lane 7</th>
+                        <th class="border border-rlss-blue w-48">Lane 8</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse ($heats->nth(2,1) as $key => $heat)
+                        <tr>
+                            <td
+                                class=" bg-rlss-blue text-white  text-center py-2 px-3 border border-rlss-blue sticky left-0">
+                                {{ $heat->first()->heat }}</td>
+                            <td
+                                class="py-2 px-3 bg-rlss-blue bg-opacity-40 text-rlss-blue border border-rlss-blue text-center">
+                                {{ $heat->where('lane', 4)->first()->getTeam->getLeague->name }}
                             </td>
-                        @endfor
-                    </tr>
-                @empty
-                    <tr>
-                        <td>No heats available yet!</td>
-                    </tr>
-                @endforelse
-            </tbody>
-        </table>
+
+                            @for ($l = 1; $l <= $comp->max_lanes; $l++)
+                                @php
+                                    $lane = $heat->where('lane', $l)->first();
+                                @endphp
+
+
+                                <td class="py-2 px-3 text-center border border-rlss-blue">
+                                    @if ($lane)
+                                        {{ $lane->getTeam->formatName(':N') }}
+                                    @else
+                                        -
+                                    @endif
+                                </td>
+                            @endfor
+                        </tr>
+                    @empty
+                        <tr>
+                            <td>No heats available yet!</td>
+                        </tr>
+                    @endforelse
+                </tbody>
+            </table>
+        </div>
+
+        <br>
+        <h2 class="font-astoria text-rlss-blue font-extrabold " id="tanks">Initiative Tanks </h2>
+
+        <div class="grid-4">
+
+            @foreach ($comp->getCompetitionTeams->groupBy('serc_tank')->sortKeys() as $tankNo => $tank)
+                <div>
+                    <h4 class=" text-rlss-red font-astoria">Tank {{ $tankNo }}</h4>
+
+                    <table class="table-auto font-greycliff">
+                        <thead>
+                            <tr class=" text-rlss-blue font-bold">
+                                <th class="bg-rlss-blue py-2 px-3 border border-rlss-blue"></th>
+                                <th class=" bg-rlss-blue bg-opacity-40 py-2 px-3 border border-rlss-blue w-48">Category
+                                </th>
+                                <th class="border border-rlss-blue w-48 ">Region</th>
+                                <th class="border border-rlss-blue w-48">Competitor</th>
+
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @forelse ($tank as $heatNo => $competitor)
+                                <tr>
+                                    <td
+                                        class=" bg-rlss-blue text-white  text-center py-2 px-3 border border-rlss-blue ">
+                                        {{ $heatNo + 1 }}</td>
+                                    <td
+                                        class="py-2 px-3 bg-rlss-blue bg-opacity-40 text-rlss-blue border border-rlss-blue text-center">
+                                        {{ $competitor->formatName(':L') }}
+                                    </td>
+
+                                    <td class="py-2 px-3 text-center border border-rlss-blue">
+                                        {{ $competitor->formatName(':R') }}
+                                    </td>
+                                    <td class="py-2 px-3 text-center border border-rlss-blue">
+                                        {{ $competitor->formatName(':N') }}
+                                    </td>
+
+                                </tr>
+                            @empty
+                                <tr>
+                                    <td>No heats available yet!</td>
+                                </tr>
+                            @endforelse
+                        </tbody>
+                    </table>
+                </div>
+            @endforeach
+
+
+
+
+        </div>
 
 
     </div>
