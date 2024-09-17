21e917431717cb774cf9e47b2805b31d7d8c270e
Noah Hollowell <noahdhollowell@googlemail.com>
2024-09-17 17:46:29 +0100

    Middleware to correctly set app.url base for dynamic hosts

--- app/Helpers/RouteHelpers.php
+++ app/Helpers/RouteHelpers.php
@@ -4,27 +4,36 @@ namespace App\Helpers;
 
 class RouteHelpers
 {
-    static function domainRemap($sub) {
+    static function domainRemap($sub)
+    {
         $defaultDomain = env('APP_SUBDOMAIN_BASE');
-    
+
         $domain = request()->getHost();
-    
-    
+
+
         $ADDITIONAL_DOMAINS = explode(",", env('APP_ADDITIONAL_DOMAINS'));
-    
+
         $ADDITIONAL_DOMAINS = array_map(function ($val) use ($sub) {
-            return $sub.$val;
+            return $sub . $val;
         }, $ADDITIONAL_DOMAINS);
-    
-      
-    
+
+
+
         if (in_array($domain, $ADDITIONAL_DOMAINS)) {
             return $domain;
         }
-    
-        return $sub.$defaultDomain;
-            
-    
+
+        return $sub . $defaultDomain;
     }
-    
+
+    static function isCustomHost(): bool
+    {
+        $domain = request()->getHost();
+
+        $hostOnly = parse_url($domain, PHP_URL_HOST);
+
+        $ADDITIONAL_DOMAINS = explode(",", env('APP_ADDITIONAL_DOMAINS'));
+
+        return in_array($hostOnly, $ADDITIONAL_DOMAINS);
+    }
 }
