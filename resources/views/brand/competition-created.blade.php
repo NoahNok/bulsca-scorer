55294b58082b062636f229e0bb7b5cfebe7d392a
Noah Hollowell <noahdhollowell@googlemail.com>
2024-09-24 17:52:42 +0100

    Brand admin competition management
    Brand amin can now create, edit and delete competitions

---
+++ resources/views/brand/competition-created.blade.php
@@ -0,0 +1,35 @@
+@extends('layout')
+
+@section('title')
+    Competition Created | Admin
+@endsection
+
+@section('breadcrumbs')
+    <div>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+            class="w-3 h-3">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+        </svg>
+        <a href="{{ route('brand.index') }}">Dashboard</a>
+
+    </div>
+    <div>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+            class="w-3 h-3">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+        </svg>
+        <a href="#">Created Competition</a>
+    </div>
+@endsection
+
+@section('content')
+    <h2 class="mb-0">Competition Created</h2>
+    <br>
+    <p>
+        <strong class="text-lg">Please copy down these details:</strong><br>
+        <strong>Email:</strong> {{ $email }} <br>
+        <strong>Password:</strong> {{ $password }}
+    </p>
+    <br>
+    <a href="{{ route('brand.comp.edit', $comp) }}" class="btn">Continue</a>
+@endsection
