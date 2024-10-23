ae94967a5f4e9acb451821f8b07f89409fe31dd1
Noah Hollowell <noahdhollowell@googlemail.com>
2024-10-23 18:20:19 +0100

    Add install manifest

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -20,6 +20,8 @@
     @endif
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <link rel="manifest" href="manifest.json">
+
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title') | BULSCA Scorer</title>
