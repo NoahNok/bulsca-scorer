d465113c4863f2ce6cf68078502bca056a72170c
Noah Hollowell <noahdhollowell@googlemail.com>
2024-11-19 16:58:20 +0000

    ensure that service worker gets registered

--- resources/views/competition/user-notifications.blade.php
+++ resources/views/competition/user-notifications.blade.php
@@ -42,7 +42,30 @@
                 }, 500)
             },
         
+            async registerServiceWorker() {
+                if ('serviceWorker' in navigator) {
+                    try {
+                        const registration = await navigator.serviceWorker.register('/sw.js', {
+                            scope: '/',
+                        });
+                        if (registration.installing) {
+                            console.log('Service worker installing');
+                        } else if (registration.waiting) {
+                            console.log('Service worker installed');
+                        } else if (registration.active) {
+                            console.log('Service worker active');
+                        }
+                    } catch (error) {
+                        console.error(`Registration failed with ${error}`);
+                    }
+                }
+            },
+        
             async initPushNotifs() {
+        
+        
+                await this.registerServiceWorker()
+        
                 if (!navigator.serviceWorker.ready) {
                     return
                 }
