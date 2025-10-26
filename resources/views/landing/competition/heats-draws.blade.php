13cd33b0f3f978ab901f6214b8ace7beba200e9d
Noah Hollowell <noahdhollowell@googlemail.com>
2025-10-26 11:30:55 +0000

    Add toggle to change heats and draws rom grid tto list view and persit that state

--- resources/views/landing/competition/heats-draws.blade.php
+++ resources/views/landing/competition/heats-draws.blade.php
@@ -24,12 +24,35 @@
     
         matchesSearch(term) {
             return term.trim().startsWith(this.search.trim())
-        }
+        },
+    
+        displayMode: $persist('grid')
     
     }">
 
-        <div class="se-form-input">
-            <input type="text" x-model="search" placeholder="Search...">
+        <div class="flex md:space-x-3 w-full items-center md:flex-row flex-col">
+            <div class="se-form-input w-full imb-0! mb-0!">
+                <input type="text" x-model="search" placeholder="Search...">
+            </div>
+
+            <div class="flex space-x-2 items-center  ">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor"
+                    class="size-8 cursor-pointer transition-colors ease-in-out hover:text-se hover:bg-gray-100 rounded-md p-1"
+                    :class="displayMode == 'grid' ? 'se-bg-se/10 text-se bg-gray-100' : 'text-gray-600'"
+                    @click="displayMode = 'grid'">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M3.75 6A2.25 2.25 0 0 1 6 3.75h2.25A2.25 2.25 0 0 1 10.5 6v2.25a2.25 2.25 0 0 1-2.25 2.25H6a2.25 2.25 0 0 1-2.25-2.25V6ZM3.75 15.75A2.25 2.25 0 0 1 6 13.5h2.25a2.25 2.25 0 0 1 2.25 2.25V18a2.25 2.25 0 0 1-2.25 2.25H6A2.25 2.25 0 0 1 3.75 18v-2.25ZM13.5 6a2.25 2.25 0 0 1 2.25-2.25H18A2.25 2.25 0 0 1 20.25 6v2.25A2.25 2.25 0 0 1 18 10.5h-2.25a2.25 2.25 0 0 1-2.25-2.25V6ZM13.5 15.75a2.25 2.25 0 0 1 2.25-2.25H18a2.25 2.25 0 0 1 2.25 2.25V18A2.25 2.25 0 0 1 18 20.25h-2.25A2.25 2.25 0 0 1 13.5 18v-2.25Z" />
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor"
+                    class="size-8 cursor-pointer transition-colors ease-in-out hover:text-se hover:bg-gray-100 rounded-md p-1"
+                    :class="displayMode == 'list' ? 'se-bg-se/10 text-se bg-gray-100' : 'text-gray-600'"
+                    @click="displayMode = 'list'">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                </svg>
+
+            </div>
         </div>
 
 
@@ -66,7 +89,7 @@
 
 
 
-            <div class="grid grid-flow-col auto-cols-max gap-4 flex-wrap overflow-x-auto snap-x snap-mandatory"
+            <div class=" hidden" :class="displayMode == 'grid' ? 'grid-4' : 'flex! flex-col space-y-'"
                 x-data="{
                 
                     children: [],
@@ -76,51 +99,54 @@
                 
                 
                     }
-                }" x-show="shouldShow">
-
-                <div class="flex flex-col  py-2 sticky top-0 left-0 bg-white pr-2 ">
-                    <h3 class="font-bold  mb-2">L</h3>
-                    <div class="space-y-2">
-
-                        @for ($i = 0; $i < $comp->max_lanes; $i++)
-                            <div class="border border-transparent font-semibold py-2  rounded">
-                                {{ $i + 1 }}
-                            </div>
-                        @endfor
+                }" x-show.important="shouldShow" x-transition>
 
 
-                    </div>
-                </div>
+
+
+
 
                 @foreach ($heatevent['heats'] as $heat_no => $lanes)
                     @php
                         $names = $lanes->map(fn($lane) => $lane->entity?->getName($comp))->filter()->implode(' ');
                     @endphp
 
-                    <div class="flex flex-col  py-2 snap-start pl-5" x-data="{
+                    <div class="flex flex-col space-x-2 " x-data="{
                         shouldShow() {
                             let show = search.trim() === '' || `{{ strtolower($names) }}`.includes(search.trim().toLowerCase())
                             children['{{ $heat_no }}'] = show
                             return show;
                         }
                     }" x-show="shouldShow">
-                        <h3 class="font-bold  mb-2">Heat {{ $heat_no }}</h3>
+
+                        <div class="flex space-x-3">
+                            <h3 class="font-bold mb-2">L</h3>
+                            <h3 class="font-bold  mb-2">Heat {{ $heat_no }}</h3>
+                        </div>
+
                         <div class="space-y-2">
 
                             @for ($i = 0; $i < $comp->max_lanes; $i++)
                                 @php
                                     $lane = $lanes->where('lane', $i + 1)->first();
                                 @endphp
-                                <div class="se-card  se-card-body min-w-56 p-2! px-4! rounded"
-                                    :class="(search.trim() !== '' &&
-                                        `{{ strtolower($lane?->entity?->getName($comp) ?? '-') }}`
-                                        .includes(search.trim().toLowerCase())) ? 'se-card-success' : ''">
-                                    {{ $lane?->entity?->getName($comp) ?? '-' }}
+                                <div class="flex items-center w-full space-x-3">
+                                    <h4 class="font-bold ">{{ $i + 1 }}</h4>
+                                    <div class="se-card  se-card-body min-w-56 p-2! px-4! rounded w-full"
+                                        :class="(search.trim() !== '' &&
+                                            `{{ strtolower($lane?->entity?->getName($comp) ?? '-') }}`
+                                            .includes(search.trim().toLowerCase())) ? 'se-card-success' : ''">
+                                        {{ $lane?->entity?->getName($comp) ?? '-' }}
+                                    </div>
                                 </div>
                             @endfor
 
 
                         </div>
+
+
+
+
                     </div>
                 @endforeach
 
@@ -149,7 +175,7 @@
 
 
 
-            <div class="grid grid-flow-row grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4    gap-4 flex-wrap">
+            <div class="hidden" :class="displayMode == 'grid' ? 'grid-4' : 'flex! flex-col space-y-1'">
 
 
 
@@ -167,7 +193,7 @@
                         
                         
                             }
-                        }" x-show="shouldShow">
+                        }" x-show="shouldShow" x-transition>
                             <h2 class="-mb-1!">Tank {{ $tank_no + 1 }}</h2>
 
                             @php
--- resources/views/layouts/core.blade.php
+++ resources/views/layouts/core.blade.php
@@ -34,6 +34,8 @@
     {{-- <link rel="stylesheet" href="{{ asset('css/app.css') }}?{{ config('version.hash') }}"> --}}
     <script defer src="https://cdn.jsdelivr.net/npm/@alpinejs/mask@3.13.3/dist/cdn.min.js"></script>
     <script defer src="https://cdn.jsdelivr.net/npm/@alpinejs/collapse@3.x.x/dist/cdn.min.js"></script>
+    <script defer src="https://cdn.jsdelivr.net/npm/@alpinejs/persist@3.x.x/dist/cdn.min.js"></script>
+
 
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.13.3/dist/cdn.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/quill@2.0.2/dist/quill.snow.css" rel="stylesheet" />
